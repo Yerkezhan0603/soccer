92321b674fcdfc8f111e83b7ec1281e8f0db56fb
Yerkezhan0603 <tolegenova0603@gmail.com>
2025-09-27 14:27:34 +0500

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
--- QUERY 1
+-- QUERY 1: Выбирает первые 10 строк из таблицы Match
 SELECT * FROM "Match" LIMIT 10;
 
--- QUERY 2
+-- QUERY 2: Матчи сезона 2014/2015, где было больше 3 голов в сумме, отсортированные по дате, максимум 50 матчей
 SELECT match_api_id, season, date, home_team_api_id, away_team_api_id, home_team_goal, away_team_goal,
        (home_team_goal + away_team_goal) AS total_goals
 FROM "Match"
@@ -9,7 +9,7 @@ WHERE season = '2014/2015' AND (home_team_goal + away_team_goal) > 3
 ORDER BY date DESC
 LIMIT 50;
 
--- QUERY 3
+-- QUERY 3: Среднее количество голов хозяев по каждой лиге, сортировка по убыванию
 SELECT l.name AS league, AVG(m.home_team_goal) AS avg_home_goals
 FROM "Match" m
 JOIN "League" l ON m.league_id = l.id
@@ -17,13 +17,13 @@ GROUP BY l.name
 ORDER BY avg_home_goals DESC
 LIMIT 10;
 
--- QUERY 4
+-- QUERY 4: Количество матчей по сезонам, отсортировано по убыванию
 SELECT season, COUNT(*) AS matches_count
 FROM "Match"
 GROUP BY season
 ORDER BY matches_count DESC;
 
--- QUERY 5
+-- QUERY 5: Подсчёт общего количества голов каждой команды, топ-20 команд
 SELECT t.team_long_name,
        SUM(
            CASE WHEN m.home_team_api_id = t.team_api_id THEN m.home_team_goal ELSE 0 END +
@@ -36,14 +36,14 @@ GROUP BY t.team_long_name
 ORDER BY total_goals DESC
 LIMIT 20;
 
--- QUERY 6
+-- QUERY 6: Средний рейтинг игроков по каждой дате, последние 20 дат
 SELECT pa.date::date AS snapshot_date, AVG(pa.overall_rating)::numeric(8,2) AS avg_overall
 FROM "Player_Attributes" pa
 GROUP BY snapshot_date
 ORDER BY snapshot_date DESC
 LIMIT 20;
 
--- QUERY 7
+-- QUERY 7: Подсчёт общего числа голов команд, топ-20
 SELECT t.team_long_name,
        SUM(
          CASE WHEN m.home_team_api_id = t.team_api_id THEN COALESCE(m.home_team_goal, 0) ELSE 0 END +
@@ -55,7 +55,7 @@ GROUP BY t.team_long_name
 ORDER BY total_goals DESC
 LIMIT 20;
 
--- QUERY 8
+-- QUERY 8: Подсчёт количества матчей с карточками и примерного общего числа карточек для каждой команды, топ-20
 SELECT t.team_long_name,
        SUM((m.card IS NOT NULL)::int) as matches_with_cards,
        SUM(COALESCE(length(m.card) - length(replace(m.card, ';', '')) + 1, 0)) AS approx_card_count
@@ -65,14 +65,14 @@ GROUP BY t.team_long_name
 ORDER BY approx_card_count DESC
 LIMIT 20;
 
--- QUERY 9
+-- QUERY 9: Подсчёт количества ударов по воротам для каждого матча, топ-20 матчей с наибольшим количеством
 SELECT match_api_id, date, home_team_api_id, away_team_api_id,
        COALESCE(length(shoton) - length(replace(shoton, ';', '')) + 1, 0) AS shots_on_target_count
 FROM "Match"
 ORDER BY shots_on_target_count DESC
 LIMIT 20;
 
--- QUERY 10
+-- QUERY 10: Среднее количество голов за матч по каждому сезону, сортировка по убыванию
 SELECT season, AVG(home_team_goal + away_team_goal) AS avg_goals
 FROM "Match"
 GROUP BY season
